082e0bd44b823d19206ac6fd3f72f703b7bd8444
Michele Carmagnani <michele.carmagnani@gmail.com>
2021-10-01 15:59:33 +0200

    adding expriationTime

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -7,16 +7,29 @@ const AuthContext = React.createContext({
   logout: () => {},
 });
 
+const calculateRemainingTime = (expirationTime) => {
+  const currentTime = new Date().getTime();
+  const adjExpTime = new Date(expirationTime).getTime()
+  const remainingTime = adjExpTime - currentTime;
+  return remainingTime
+}
+
 export const AuthContextProvider = (props) => {
-  const [token, setToken] = useState(null)
+  const initialToken = localStorage.getItem('token')
+  const [token, setToken] = useState(initialToken);
 
   const userIsLoggedIn = !!token;
 
-  const loginHandler = (token) => { setToken(token);}
+  
   const logoutHandler = () => {
     setToken(null);
+    localStorage.removeItem('token');
   }
-
+const loginHandler = (token, exirationTime) => {
+  setToken(token);
+  localStorage.setItem("token", token);
+  const remainingTime = calculateRemainingTime(exirationTime);
+};
   const contextValue = {
     taken: token,
     isLoggedIn: userIsLoggedIn,
